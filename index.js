37178c597e6d7ae28657f17a0616b7dff97195af
Erik Wong <ewong@blizzard.com>
2016-03-17 20:37:08 -0700

    Add rest of scripts

---
+++ index.js
@@ -0,0 +1,36 @@
+var five = require('johnny-five');
+var board = new five.Board();
+var express = require('express');
+
+var rgb;
+board.on('ready', function() {
+  rgb = new five.Led.RGB([11, 10, 9]);
+});
+
+
+var app = express();
+
+app.get('/events/build/failure', function(req, res) {
+  rgb.color('#ff0000');
+  res.json({
+    color: 'red'
+  })
+});
+
+app.get('/events/build/error', function(req, res) {
+  rgb.color('#00ff00');
+  res.json({
+    color: 'green'
+  })
+});
+
+app.get('/event/build/', function(req, res) {
+  rgb.color('#ffff00');
+  res.json({
+    color: 'yellow'
+  })
+});
+
+var server = app.listen(9000, function() {
+  console.log('sever');
+});
